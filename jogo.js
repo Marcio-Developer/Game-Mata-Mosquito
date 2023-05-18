44c08123a48a97a049a8110d156ab332a56f838b
Marcio Silva <marciobolari@hotmail.com>
2023-05-18 00:10:38 -0300

    Alterações

--- jogo.js
+++ jogo.js
@@ -55,7 +55,7 @@ function posicaoRandomica() {
 
             window.location.href = 'fim_de_jogo.html'
         } else {
-            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            document.getElementById('v' + vidas).src = "Img/coracao_vazio.png"
 
             vidas++
         }
@@ -71,7 +71,7 @@ function posicaoRandomica() {
 
     //criar o elemento html
     var mosquito = document.createElement('img')
-    mosquito.src = 'imagens/mosquito.png'
+    mosquito.src = 'Img/mosquito.png'
     mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
